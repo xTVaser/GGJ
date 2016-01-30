10434cc4f12d6a1923cb32fcb7f6f073b15162fb
Tyler <xtvaser@gmail.com>
2016-01-30 11:55:00 -0500

    made some changes

--- game.js
+++ game.js
@@ -5,10 +5,10 @@ function preload() {
     game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tileset1', 'assets/tileset1.png');
     game.load.spritesheet('dude', 'assets/guyset1.png', 32, 40);
-    game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
+    game.load.spritesheet('enemy', 'assets/enemytileset.png', 32, 40);
     game.load.image('starSmall', 'assets/star.png');
     game.load.image('starBig', 'assets/star2.png');
-    game.load.image('background', 'assets/background2.png');
+    game.load.image('background', 'assets/tilebackground.png');
 
 }
 
@@ -16,7 +16,7 @@ var map;
 var tileset;
 var layer1;
 var layer2;
-var player;
+var player, dood;
 var facing = 'left';
 var jumpTimer = 0;
 var cursors;
@@ -30,7 +30,7 @@ cursors = game.input.keyboard.createCursorKeys();
 
     game.stage.backgroundColor = '#000000';
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+    bg = game.add.tileSprite(0, 0, 10000, 600, 'background');
     bg.fixedToCamera = true;
 
     map = game.add.tilemap('level1');
@@ -43,7 +43,7 @@ cursors = game.input.keyboard.createCursorKeys();
     layer2 = map.createLayer('grass');
 
     //  Un-comment this on to see the collision tiles
-    layer.debug = true;
+    //layer.debug = true;
 
     layer.resizeWorld();
 
@@ -54,15 +54,24 @@ cursors = game.input.keyboard.createCursorKeys();
 
     player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 10);
+    player.body.setSize(20, 32, 5, 8);
 
     player.animations.add('left', [3, 2, 1, 0], 10, true);
     player.animations.add('turn', [4], 20, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    dood = game.add.sprite(200, 0, 'enemy');
+    game.physics.enable(dood, Phaser.Physics.ARCADE);
+
+    dood.body.bounce.y = 0.2;
+    dood.body.collideWorldBounds = true;
+    dood.body.setSize(20, 32, 5, 10);
+
+    dood.animations.add('left', [3, 2, 1, 0], 10, true);
+    dood.animations.add('turn', [4], 20, true);
+    dood.animations.add('right', [5, 6, 7, 8], 10, true);
 
 
-    game.camera.follow(player);
 
 
 
@@ -72,6 +81,7 @@ cursors = game.input.keyboard.createCursorKeys();
 function update() {
 
     game.physics.arcade.collide(player, layer);
+    game.physics.arcade.collide(dood, layer);
 
     player.body.velocity.x = 0;
 
@@ -116,7 +126,7 @@ function update() {
         }
     }
 
-    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+    if ((jumpButton.isDown || cursors.up.isDown) && player.body.onFloor() && game.time.now > jumpTimer)
     {
         player.body.velocity.y = -250;
         jumpTimer = game.time.now + 750;
