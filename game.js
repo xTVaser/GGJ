448551ef70ef22fa319de57d4ba25c36fb8722c7
Robert Brown <rbrown@localhost.localdomain>
2016-01-30 14:54:00 -0500

    Added item collisions

--- game.js
+++ game.js
@@ -29,6 +29,11 @@ var cursors;
 var jumpButton;
 var bg;
 var deathPlane;
+var neckPickup;
+var feaPickup;
+var vooPickup;
+var medPickup;
+var bookPickup;
 
 var feather, book, medicinepouch, necklace, voodoo;
 var item;
@@ -58,9 +63,9 @@ function create() {
 
     layer.resizeWorld();
 
-    game.physics.arcade.gravity.y = 600;
+    game.physics.arcade.gravity.y = 450;
 
-    player = game.add.sprite(200, 0, 'dude');
+    player = game.add.sprite(200, 585, 'dude');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.bounce.y = 0.2;
@@ -83,16 +88,31 @@ function create() {
     dood.animations.add('right', [5, 6, 7, 8], 10, true);
     game.camera.follow(player);
 
-    addSprite(903,544,'necklace',necklace,20,32,5,8);
-    addSprite(2200,810,'feather',feather,20,32,5,8);
-    addSprite(3850,32,'voodoo',voodoo,20,32,5,8);
-    addSprite(4875,1216,'medicinepouch',medicinepouch,20,32,5,8);
-    addSprite(6753,672,'book',book,20,32,5,8);
+    //addSprite(903,544,'necklace',necklace,20,32,5,8);
+    necklace = game.add.sprite(903,544,'necklace');
+    game.physics.enable(necklace, Phaser.Physics.ARCADE);
+    necklace.body.collideWorldBounds = true;
+    //addSprite(2200,810,'feather',feather,20,32,5,8);
+    feather = game.add.sprite(2200,810,'feather');
+    game.physics.enable(feather, Phaser.Physics.ARCADE);
+    feather.body.collideWorldBounds = true;
+    //addSprite(3850,32,'voodoo',voodoo,20,32,5,8);
+    voodoo = game.add.sprite(3850,32,'voodoo');
+    game.physics.enable(voodoo, Phaser.Physics.ARCADE);
+    voodoo.body.collideWorldBounds = true;
+    //addSprite(4875,1216,'medicinepouch',medicinepouch,20,32,5,8);
+    medicinepouch = game.add.sprite(4875,1216,'medicinepouch');
+    game.physics.enable(medicinepouch, Phaser.Physics.ARCADE);
+    medicinepouch.body.collideWorldBounds = true;
+    //addSprite(6753,672,'book',book,20,32,5,8);
+    book = game.add.sprite(6753,672,'book');
+    game.physics.enable(book, Phaser.Physics.ARCADE);
+    book.body.collideWorldBounds = true;
 }
 
 function addSprite(x, y, sprite, variable, sizeX, sizeY, sizeX2, sizeY2) {
         variable = game.add.sprite(x, y, sprite);
-        //game.physics.enable(variable, Phaser.Physics.ARCADE);
+
 
         variable.body.bounce.y = 0.2;
         variable.body.collideWorldBounds = true;
@@ -101,11 +121,46 @@ function addSprite(x, y, sprite, variable, sizeX, sizeY, sizeX2, sizeY2) {
 function update() {
     game.physics.arcade.collide(player, layer);
     game.physics.arcade.collide(dood, layer);
+    game.physics.arcade.collide(necklace, layer);
+    game.physics.arcade.collide(feather, layer);
+    game.physics.arcade.collide(voodoo, layer);
+    game.physics.arcade.collide(medicinepouch, layer);
+    game.physics.arcade.collide(book, layer);
+
+    if (game.physics.arcade.collide(player, necklace) == true)
+    {
+      necklace.destroy(true);
+      neckPickup = true;
+    }
+
+    if (game.physics.arcade.collide(player, feather) == true)
+    {
+      feather.destroy(true);
+      feaPickup = true;
+    }
+
+    if (game.physics.arcade.collide(player, voodoo) == true)
+    {
+      voodoo.destroy(true);
+      vooPickup = true;
+    }
+
+    if (game.physics.arcade.collide(player, medicinepouch) == true)
+    {
+      medicinepouch.destroy(true);
+      medPickup = true;
+    }
+
+    if (game.physics.arcade.collide(player, book) == true)
+    {
+      book.destroy(true);
+      bookPickup = true;
+    }
 
     player.body.velocity.x = 0;
 
     //Check if too low
-    if (player.body.y > 1700) {
+    if (player.body.y > 1400) {
         //die
         player.x = 200;
         player.y = 120;
